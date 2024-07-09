4692833ef66e6815964cb85a4c75c5700ad69edf
Gustavo <gusstavocta@gmail.com>
2024-07-08 22:44:45 -0500

    implementacion update

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,8 +6,8 @@ import {
   Patch,
   Param,
   Delete,
-  ParseUUIDPipe,
   Query,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -27,20 +27,19 @@ export class ProductsController {
   //! find all products
   @Get()
   findAll(@Query() paginationDto: PaginationDto) {
-    
     return this.productsService.findAll(paginationDto);
   }
 
   //! find one product
   @Get(':term')
-  findOne(@Param('term', ) term: string) {
+  findOne(@Param('term') term: string) {
     return this.productsService.findOne(term);
   }
 
   //! update product
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+  update(@Param('id', ParseUUIDPipe) id: string, @Body() updateProductDto: UpdateProductDto) {
+    return this.productsService.update(id, updateProductDto);
   }
 
   //! remove product
