6d8dfc7cc986ea45ee7a81ab0cd2dfc1427ebb3d
Gustavo <gusstavocta@gmail.com>
2024-07-15 23:16:08 -0500

    agregando comentario

--- src/files/helpers/file.Filter.helper.ts
+++ src/files/helpers/file.Filter.helper.ts
@@ -1,19 +1,22 @@
-import { BadRequestException } from "@nestjs/common";
+import { BadRequestException } from '@nestjs/common';
 
+//! fileFilter function is used to filter the files that are being uploaded to the server.
 export const fileFilter = (
   req: Express.Request,
   file: Express.Multer.File,
-  callback: Function,
+  callback: (error: Error | null, acceptFile: boolean) => void, // callback function
 ) => {
   if (!file) return callback(new Error('No file uploaded'), false);
   const fileExtension = file.mimetype.split('/')[1];
   const validExtensions = ['jpg', 'jpeg', 'png', 'gif'];
-    if (!validExtensions.includes(fileExtension)) {
-        return callback(
-        new BadRequestException('Invalid file type. Only jpg, jpeg, png, gif files are allowed'),
-        false,
-        );
-    }
- 
+  if (!validExtensions.includes(fileExtension)) {
+    return callback(
+      new BadRequestException(
+        'Invalid file type. Only jpg, jpeg, png, gif files are allowed',
+      ),
+      false,
+    );
+  }
+
   callback(null, true);
 };
