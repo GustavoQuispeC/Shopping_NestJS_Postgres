2f4870b9446a07b7645fd7e326195de51db4e1c2
Gustavo <gusstavocta@gmail.com>
2024-07-16 23:52:06 -0500

    generando url de imagen

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,10 +1,14 @@
 import {
   BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
+import { Response } from 'express';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter } from './helpers/file.Filter.helper';
@@ -15,6 +19,19 @@ import { fileNamer } from './helpers/file.Namer.helper';
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
+  @Get(`product/:imageName`)
+
+  findProductImage(
+    @Res() res: Response, // response object
+    @Param('imageName') imageName: string) {
+    const path = this.filesService.getStaticProductImage(imageName);
+    // res.status(403).json({
+    //   ok: false,
+    //   path: path,
+    // });
+    res.sendFile(path);
+  }
+
   @Post('product')
   @UseInterceptors(
     FileInterceptor('file', {
@@ -22,7 +39,7 @@ export class FilesController {
       //limits: { fileSize: 1024 * 1024 },
       storage: diskStorage({
         destination: './static/products',
-        filename : fileNamer
+        filename: fileNamer,
       }),
     }),
   )
@@ -30,9 +47,10 @@ export class FilesController {
     if (!file) {
       throw new BadRequestException('make sure that file is an image');
     }
+    const secureUrl = `${file.filename}`;
 
     return {
-      filename: file.originalname,
+      secureUrl,
     };
   }
 }
