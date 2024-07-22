b8359242478171de7358d9fdef533d94b639c8cb
Gustavo <gusstavocta@gmail.com>
2024-07-22 00:46:13 -0500

    fin de la fase carga de imagenes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,13 @@ import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
-
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
-   
     ConfigModule.forRoot(),
-   
+
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
@@ -21,17 +21,18 @@ import { FilesModule } from './files/files.module';
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true,
-      
     }),
-   
+  //! serve static files from the public folder
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
     ProductsModule,
-   
+
     CommonModule,
-   
+
     SeedModule,
-   
+
     FilesModule,
   ],
-  
 })
 export class AppModule {}
